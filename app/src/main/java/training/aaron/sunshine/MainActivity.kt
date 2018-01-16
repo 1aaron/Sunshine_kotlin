7f7a6cf6b532b32eedf05827fc26a15d389a9c92
aaron <aaronnek1@gmail.com>
2018-01-16 09:42:35 -0600

    implementation of styles and initial approach to ui

--- app/src/main/java/training/aaron/sunshine/MainActivity.kt
+++ app/src/main/java/training/aaron/sunshine/MainActivity.kt
@@ -2,11 +2,15 @@ package training.aaron.sunshine
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(tootlbar)
+        setTitle("prueba de titulo")
     }
 }
